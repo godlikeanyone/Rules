20898b2bc9312c541203e9af1d8be4ca3ee27431
godlikeanyone <1093496273@qq.com>
2019-11-20 16:08:22 +0800

    Update Symbolab.js

--- Symbolab.js
+++ Symbolab.js
@@ -1,10 +1,6 @@
 /**
  * @supported 82AC21D8
  */
-var body = $response.body;
-
-let obj = JSON.parse(body);
-obj.valid = true;
-obj.newlyAssociated = true;
-body = JSON.stringify(obj);
-$done({body});
+let obj = JSON.parse($response.body);
+obj = {"valid":true,"newlyAssociated": true};
+$done({body: JSON.stringify(obj)});
