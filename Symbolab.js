18ef368b19d4c8cb28a7119ce503884ef1d60402
godlikeanyone <1093496273@qq.com>
2019-11-20 15:26:56 +0800

    Update Symbolab.js

--- Symbolab.js
+++ Symbolab.js
@@ -2,16 +2,9 @@
  * @supported 82AC21D8
  */
 var body = $response.body;
-var url = $request.url;
-
-const path1 = 'appleSubscriptionValidate';
 
 let obj = JSON.parse(body);
-
-if (url.indexOf(path1) != -1) {
-	obj.valid = true;
-	obj.newlyAssociated = true;
-	body = JSON.stringify(obj);  
- }
-
+obj.valid = true;
+obj.newlyAssociated = true;
+body = JSON.stringify(obj);
 $done({body});
