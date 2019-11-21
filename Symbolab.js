7479727b6e139273d718efa197bbf15f8b94ad6c
godlikeanyone <1093496273@qq.com>
2019-11-21 10:55:11 +0800

    Update Symbolab.js

--- Symbolab.js
+++ Symbolab.js
@@ -2,5 +2,7 @@
  * @supported 82AC21D89933
  */
 let obj = JSON.parse($response.body);
-obj = {"valid":true,"newlyAssociated":true,"expired":false};
+obj.udid = "doeijd3-dnoi3-39029309dj3jd20";
+obj.avatarStockChoice = 1;
+obj.firstName = "AA";
 $done({body: JSON.stringify(obj)});
