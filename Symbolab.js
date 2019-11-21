20a63bbbce916f77386b8b1c035e5778907c265f
godlikeanyone <1093496273@qq.com>
2019-11-21 10:57:05 +0800

    Update Symbolab.js

--- Symbolab.js
+++ Symbolab.js
@@ -1,7 +1,7 @@
 /**
  * @supported 82AC21D89933
  */
-let obj = JSON.parse($response.body);
+let obj = JSON.parse($request.body);
 obj.udid = "doeijd3-dnoi3-39029309dj3jd20";
 obj.avatarStockChoice = 1;
 obj.firstName = "AA";
