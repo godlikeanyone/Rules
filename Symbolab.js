31728b073db9a10b1053c13f7d5a3d1c4510b242
godlikeanyone <1093496273@qq.com>
2019-11-21 11:02:25 +0800

    Delete Symbolab.js

--- Symbolab.js
+++
@@ -1,8 +0,0 @@
-/**
- * @supported 82AC21D89933
- */
-let obj = JSON.parse($request.body);
-obj.udid = "doeijd3-dnoi3-39029309dj3jd20";
-obj.avatarStockChoice = 1;
-obj.firstName = "AA";
-$done({body: JSON.stringify(obj)});
