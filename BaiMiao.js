71cc62dede3450e21b3e76999a65f3f6ed849bac
godlikeanyone <1093496273@qq.com>
2019-12-03 15:01:33 +0800

    Update BaiMiao.js

--- BaiMiao.js
+++ BaiMiao.js
@@ -4,5 +4,6 @@ obj.value.recognize.remainNormal = -100;
 obj.value.recognize.remainBatch = -100;
 obj.value.recognize.remainTranslate = -100;
 obj.value.recognize.recognizeTranslateAll = 1;
+obj.value.vip = true;
 body = JSON.stringify(obj);
 $done({body});
