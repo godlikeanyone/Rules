cc67fa696e5c46825ef08cf0db528782d263e407
godlikeanyone <1093496273@qq.com>
2019-12-03 14:57:17 +0800

    Create BaiMiao.js

---
+++ BaiMiao.js
@@ -0,0 +1,8 @@
+var body = $response.body;
+let obj = JSON.parse(body);
+obj.value.recognize.remainNormal = -100;
+obj.value.recognize.remainBatch = -100;
+obj.value.recognize.remainTranslate = -100;
+obj.value.recognize.recognizeTranslateAll = 1;
+body = JSON.stringify(obj);
+$done({body});
