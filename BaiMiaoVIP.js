bacba0ecb48118a90121068a21317bad4886c3bd
godlikeanyone <1093496273@qq.com>
2019-12-04 10:17:21 +0800

    Delete BaiMiaoVIP.js

--- BaiMiaoVIP.js
+++
@@ -1,8 +0,0 @@
-var body = $response.body;
-let obj = JSON.parse(body);
-obj.value.defaultRecognize.defaultNormal = -100;
-obj.value.defaultRecognize.defaultBatch = -100;
-obj.value.defaultRecognize.defaultTranslate = -100;
-obj.value.defaultRecognize.recognizeTranslateAll = 1;
-body = JSON.stringify(obj);
-$done({ body });
