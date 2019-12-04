577dadfcb07d0ec14701d6087d9600b05cf4f5e1
godlikeanyone <1093496273@qq.com>
2019-12-04 10:17:28 +0800

    Delete BaiMiao.js

--- BaiMiao.js
+++
@@ -1,9 +0,0 @@
-var body = $response.body;
-let obj = JSON.parse(body);
-obj.value.recognize.remainNormal = -100;
-obj.value.recognize.remainBatch = -100;
-obj.value.recognize.remainTranslate = -100;
-obj.value.recognize.recognizeTranslateAll = 1;
-obj.value.vip = true;
-body = JSON.stringify(obj);
-$done({body});
