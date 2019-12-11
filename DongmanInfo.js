0f619ae57b0a0e701221d7e0798cfe156df97352
godlikeanyone <1093496273@qq.com>
2019-12-11 17:06:46 +0800

    Update DongmanInfo.js

--- DongmanInfo.js
+++ DongmanInfo.js
@@ -1,1 +1,8 @@
-
+var body = $response.body;
+var url = $request.url;
+let obj = JSON.parse(body);
+obj.message.result.episodeInfo.price = 0;
+obj.message.result.episodeInfo.isPurchased = true;
+obj.message.result.episodeInfo.isWithPay = true;
+body = JSON.stringify(obj);
+$done({body});
