1d221718b4d0f866a301a7751841f0078421a97b
godlikeanyone <1093496273@qq.com>
2019-12-12 00:07:36 +0800

    Delete DongmanInfo.js

--- DongmanInfo.js
+++
@@ -1,8 +0,0 @@
-var body = $response.body;
-var url = $request.url;
-let obj = JSON.parse(body);
-obj.message.result.episodeInfo.price = 0;
-obj.message.result.episodeInfo.isPurchased = true;
-obj.message.result.episodeInfo.isWithPay = true;
-body = JSON.stringify(obj);
-$done({body});
