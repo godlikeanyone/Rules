e6ae2b00ad520ee6c1eef88f0b7ed11a53cae83d
godlikeanyone <1093496273@qq.com>
2021-07-08 16:49:50 +0800

    Update ColorWeather.js

--- ColorWeather.js
+++ ColorWeather.js
@@ -1,10 +1,10 @@
+  
 var body = $response['body'];
 var obj = JSON['parse'](body);
-obj['result']['xy_vip_expire'] = 970844390;
-obj['result']['is_vip'] = true;
-obj['result']['vip_expired_at'] = 970844390;
-obj['result']['is_xy_vip'] = true;
-obj['result']['vip_type'] = "s";
+obj['result']['free_trial'] = 0;
+obj['result']['vip_type'] = '';
+obj['result']['svip_expired_at'] = 0;
+obj['result']['is_vip'] = false;
 body = JSON['stringify'](obj);
 $done({
     body
