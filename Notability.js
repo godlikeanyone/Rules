66ef3dd3d86f9e5b499ef21722e4efe9be2593e7
Godlike <26649232+godlikeanyone@users.noreply.github.com>
2022-12-18 19:04:43 -0500

    Update Notability.js

--- Notability.js
+++ Notability.js
@@ -1,24 +1,22 @@
 let obj = JSON.parse($response.body);
 obj = {
-	'data': {
-		'processAppleReceipt': {
-			'__typename': 'SubscriptionResult',
-			'error': 0,
-			'subscription': {
-				'__typename': 'AppStoreSubscription',
-				'status': 'active',
-				'originalPurchaseDate': '2021-11-01T17:55:01.000Z',
-				'originalTransactionId': '1',
-				'expirationDate': '9999-12-30T18:55:00.000Z',
-				'productId': 'com.gingerlabs.Notability.premium_subscription',
-				'tier': 'premium',
-				'refundedDate': null,
-				'refundedReason': null,
-				'user': null
-			}
-		}
-	}
+  "data": {
+    "processAppleReceipt": {
+      "__typename": "SubscriptionResult",
+      "error": 0,
+      "subscription": {
+        "__typename": "AppStoreSubscription",
+        "status": "active",
+        "originalPurchaseDate": "2021-11-02T08:04:39.000Z",
+        "originalTransactionId": "7",
+        "expirationDate": "2099-09-09T09:04:39.000Z",
+        "productId": "com.gingerlabs.Notability.premium_subscription",
+        "tier": "premium",
+        "refundedDate": null,
+        "refundedReason": null,
+        "user": null
+      }
+    }
+  }
 };
-$done({
-	'body': JSON.stringify(obj)
-})
+$done({body: JSON.stringify(obj)});
